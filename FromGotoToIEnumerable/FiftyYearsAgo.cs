e4fa734ac8db81a1e5d3b682d0abaa636cee8967
Stefan Alpers <stefan.alpers@its-service.de>
2019-09-03 12:52:25 +0200

    loop with goto

---
+++ FromGotoToIEnumerable/FiftyYearsAgo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FromGotoToIEnumerable
+{
+    internal static class FiftyYearsAgo
+    {
+        internal static void LoopWithGoto()
+        {
+            int i = 0;
+        printloop:
+            Console.WriteLine(i);
+            i++;
+            if (i < 10)
+            {
+                goto printloop;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FromGotoToIEnumerable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FromGotoToIEnumerable
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            FiftyYearsAgo.LoopWithGoto();
+        }
+    }
+}
